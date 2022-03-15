7e7fa3435a4b55d7efa2120904d953d0f428ed07
Hendi Santika <hendisantika@gmail.com>
2022-03-16 06:40:02 +0700

    #6 - Add EmployeeServiceImpl

---
+++ src/main/java/com/hendisantika/service/EmployeeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-unit-test-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/03/22
+ * Time: 06.39
+ */
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+}
