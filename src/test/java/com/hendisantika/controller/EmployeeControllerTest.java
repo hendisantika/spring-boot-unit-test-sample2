496975db4134deab49400d45e72ac55abdf5212b
Hendi Santika <hendisantika@gmail.com>
2022-03-16 06:47:19 +0700

    #19 - Add EmployeeControllerTest

---
+++ src/test/java/com/hendisantika/controller/EmployeeControllerTest.java
@@ -0,0 +1,29 @@
+package com.hendisantika.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-unit-test-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/03/22
+ * Time: 06.46
+ */
+@WebMvcTest
+class EmployeeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+}
