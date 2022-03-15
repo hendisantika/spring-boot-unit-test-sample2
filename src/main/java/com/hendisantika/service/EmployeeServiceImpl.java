0998f265591eb38c02f7ce25e955e51a055383b8
Hendi Santika <hendisantika@gmail.com>
2022-03-16 06:42:11 +0700

    #9 - Add getEmployeeById in EmployeeServiceImpl

--- src/main/java/com/hendisantika/service/EmployeeServiceImpl.java
+++ src/main/java/com/hendisantika/service/EmployeeServiceImpl.java
@@ -38,4 +38,9 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    @Override
+    public Optional<Employee> getEmployeeById(long id) {
+        return employeeRepository.findById(id);
+    }
 }
