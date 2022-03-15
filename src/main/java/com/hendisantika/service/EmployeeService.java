3b88417f7bbf1a8c6b8db122aa78e9890e49a921
Hendi Santika <hendisantika@gmail.com>
2022-03-16 06:39:31 +0700

    #5 - Add Employee Service

---
+++ src/main/java/com/hendisantika/service/EmployeeService.java
@@ -0,0 +1,27 @@
+package com.hendisantika.service;
+
+import com.hendisantika.entity.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-unit-test-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/03/22
+ * Time: 06.39
+ */
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    Optional<Employee> getEmployeeById(long id);
+
+    Employee updateEmployee(Employee updatedEmployee);
+
+    void deleteEmployee(long id);
+}
